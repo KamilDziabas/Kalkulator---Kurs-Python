6898703c78ee54ee178a1b5eb6215abcdd8cbe74
KamilDziabas <100954757+KamilDziabas@users.noreply.github.com>
2022-03-04 17:20:28 +0100

    Add files via upload

---
+++ Kalkulator.py
@@ -0,0 +1,21 @@
+#Napisz program który wczyta dwie liczby wprowadzone
+#przez użytkownika. Następnie wykonaj operacje:
+#dodawania, odejmowania, mnożenia oraz dzielenia
+#podanych liczb. Po wykonaniu każdej operacji wypisz jej wynik.
+print("Podaj pierwszą liczbę: ")
+a = int(input())
+print("Podaj drugą liczbę: ")
+b = int(input())
+dodawanie = a + b
+print("Wynikiem dodawania jest ")
+print(dodawanie)
+odejmowanie = a - b
+print("Wynikiem odejmowania jest ")
+print(odejmowanie)
+mnozenie = a * b
+print("Wynikiem mnożenia jest ")
+print(mnozenie)
+dzielenie = a / b
+print("Wynikiem dzielenia jest ")
+print(dzielenie)
+
